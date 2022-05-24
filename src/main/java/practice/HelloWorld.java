8fdabb61ccb2547658c2c3b652bf9cc9fdb30831
gwl <gwl@DESKTOP-4BVJKLC>
2022-05-24 19:41:34 +0530

    Latest changes-Nancy

--- src/main/java/practice/HelloWorld.java
+++ src/main/java/practice/HelloWorld.java
@@ -3,5 +3,11 @@ package practice;
 public class HelloWorld {
 //Nancy
 	
+	public static void main(String args[]){
+		System.out.println("This is first program for the practice" );
+	}
+	
 	/* Test Project
+	 * */
+	//xyz
 }
