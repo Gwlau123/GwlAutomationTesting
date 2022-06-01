54c97c10bd96ab7e59daeeaa0a18151cdfbefa0f
gwl <gwl@DESKTOP-4BVJKLC>
2022-06-01 21:06:04 +0530

    variables

--- src/main/java/datatypes/Datatype.java
+++ src/main/java/datatypes/Datatype.java
@@ -1,9 +1,11 @@
 package datatypes;
 
-public class Integer {
+public class Datatype {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
 	}
 
---
+++ src/main/java/datatypes/Variables.java
@@ -0,0 +1,19 @@
+package datatypes;
+
+public class Variables {
+	
+private final static int WAIT_CONST =30;
+ private int  instanceVar; 
+ 
+ private static int classVar;
+ 
+ 
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int localVar;
+		
+
+	}
+
+}
