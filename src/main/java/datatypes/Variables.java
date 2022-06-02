07ce758fdb9c20512323658df12b981a87970a01
Shailendra Singh Rajawat <shailsingh.rajawat@gmail.com>
2022-06-02 15:39:59 +0530

    Variables Example

--- src/main/java/datatypes/Variables.java
+++ src/main/java/datatypes/Variables.java
@@ -1,28 +1,39 @@
 package datatypes;
 
-public class Variables 
-{
-static int m=100;//static variable  
+public class Variables {
 
+	private final static int WAIT_CONST = 30;
 
-	
-//private final static int WAIT_CONST =30;
-// private int  instanceVar; 
- 
- //private static int classVar;
- 
- void method()  
- {    
-     int n=90;//local variable   
- }
- 
+	private int instanceVar;
+
+	private static int classVar;
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		int localVar;
-		
-		     int data=50;//instance variable    
-		    }  
-		}//end of class   
-	
 
+		// Accessing, setting and getting value in LOCAL variable
+		localVar = 200;
+		System.out.println("Local Var "+ localVar);
 
+		// Accessing, setting and getting value in STATIC variable
+		Variables.classVar = 100;
+		System.out.println("Class Var "+ Variables.classVar);
+		System.out.println("Constant Var "+ Variables.WAIT_CONST);
+
+		// Accessing, setting and getting value in OBJECT/INSTANCE variable
+
+		Variables obj1 = new Variables();
+		obj1.instanceVar = 500;
+		System.out.println("Instance Var "+ obj1.instanceVar);
+		obj1.classVar = 600;
+		System.out.println("Class Var "+ obj1.classVar);
+
+		Variables obj2 = new Variables();
+		obj2.instanceVar = 200;
+		System.out.println("Instance Var "+ obj2.instanceVar);
+		System.out.println("Class Var "+ obj2.classVar);
+		System.out.println("Class Var "+ Variables.classVar);
+		System.out.println("Class Var "+ obj1.classVar);
+	}
+
+}
