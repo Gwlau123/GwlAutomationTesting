e8e36f9d00798ffcff3445cd2bf6386151056874
gwl <gwl@DESKTOP-4BVJKLC>
2022-06-02 12:05:28 +0530

    updates

--- src/main/java/datatypes/Datatype.java
+++ src/main/java/datatypes/Datatype.java
@@ -1,9 +1,24 @@
 package datatypes;
 
 public class Datatype {
-
+	/*int myNum = 5;               // Integer (whole number)
+	float myFloatNum = 5.99f;    // Floating point number
+	char myLetter = 'D';         // Character
+	boolean myBool = true;       // Boolean
+	String myText = "Hello";     // String
+	*/
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int myNum = 5;               // Integer (whole number)
+		float myFloatNum = 5.99f;    // Floating point number
+		char myLetter = 'D';         // Character
+		boolean myBool = true;       // Boolean
+		String myText = "Hello";     // String
+		System.out.println(myNum);
+	    System.out.println(myFloatNum);
+	    System.out.println(myLetter);
+	    System.out.println(myBool);
+	    System.out.println(myText);
 		
 		
 
