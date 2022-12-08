f5d27ef44683255fb98ae2e34c8973c5c6a2168e
MeemansaPathak <meemansa.pathak@galaxyweblinks.in>
2022-12-08 13:27:30 +0530

    Chrome setup changes

--- src/main/java/selenium_form_interaction/RadioButton1.java
+++ src/main/java/selenium_form_interaction/RadioButton1.java
@@ -3,13 +3,18 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 public class RadioButton1 {
 	public static void main(String[] args) throws InterruptedException {									
 		
     	// declaration and instantiation of objects/variables		
-    	System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");					
-        WebDriver driver = new ChromeDriver();
-        driver.manage().window().maximize();
+    	//System.setProperty("webdriver.chrome.driver","C:\\Program Files (x86)\\Google\\Chrome\\Application\\chromedriver.exe");					
+        //WebDriver driver = new ChromeDriver();
+        //driver.manage().window().maximize();
+		 WebDriverManager.chromedriver().setup();
+		   WebDriver driver = new ChromeDriver();
+		    driver.manage().window().maximize();
         Thread.sleep(3000);
         		
         String baseUrl = "https://demo.automationtesting.in/Register.html";				
